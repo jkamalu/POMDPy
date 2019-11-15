b2a0f3c3115828991eb86e0cbef3ec7292d9ed61
jkamalu <john.kamalu@gmail.com>
2019-11-14 20:46:03 -0800

    observation indices and util functions

--- examples/traffic_light/util.py
+++ examples/traffic_light/util.py
@@ -1,6 +1,12 @@
-import math
-import os.path
 import random
+import nump as np
+
+import json
+
+config = json.load("config.json")
+
+MIN_WAVELENGTH = min(config["color_means"] - 2 * config["color_stdev"])
+MAX_WAVELENGTH = max(config["color_means"] + 2 * config["color_stdev"])
 
 class Acceleration(Enum):
     NEGATIVE_LRG = -8
@@ -20,5 +26,20 @@ class LightColor(Enum):
 
 def pdf(mean, std, value):
     u = float(value - mean) / abs(std)
-    y = (1.0 / (math.sqrt(2 * math.pi) * abs(std))) * math.exp(-u * u / 2.0)
+    y = (1.0 / (np.sqrt(2 * np.pi) * abs(std))) * np.exp(-u * u / 2.0)
     return y
+
+def observation_to_index(obs):
+    assert len(obs) == 2
+    wavelength, distance = obs
+    return np.ravel_multi_index(
+        (wavelength - MIN_WAVELENGTH, distance),
+        (MAX_WAVELENGTH - MIN_WAVELENGTH, config["road_length"])
+    )
+
+def index_to_observation(idx):
+    wavelength, distance = np.unravel_index(
+        idx,
+        (MAX_WAVELENGTH - MIN_WAVELENGTH, config["road_length"])
+    )
+    return wavelength + MIN_WAVELENGTH, distance
